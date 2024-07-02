92a1fe04c91905513729fd74aef8bbbf63af693b
mohammedajmal <mdajmalmt@gmail.com>
2024-07-02 15:15:16 +0530

    home page changes

--- src/components/home/destinations.jsx
+++ src/components/home/destinations.jsx
@@ -15,32 +15,38 @@ const DestinationsData = [
   {
     title: 'South Korea',
     image: 'https://images.unsplash.com/photo-1551249506-d8e2c5536f8a?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    description: "South Korea beckons travelers with its captivating blend of ancient traditions, modern cities, and natural wonders. From the vibrant streets of Seoul to the serene beauty of Jeju Island, South Korea offers a diverse range of experiences. Its unique blend of history, technology, and natural beauty make it a top destination for those seeking a dynamic and enriching travel experience."
+    // description: "South Korea beckons travelers with its captivating blend of ancient traditions, modern cities, and natural wonders. From the vibrant streets of Seoul to the serene beauty of Jeju Island, South Korea offers a diverse range of experiences. Its unique blend of history, technology, and natural beauty make it a top destination for those seeking a dynamic and enriching travel experience."
+    description: "Coming soon"
   },
   {
     title: 'Switzerland',
     image: 'https://images.unsplash.com/photo-1528493366314-e317cd98dd52?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    description: "Switzerland captivates visitors with its breathtaking landscapes, from the majestic Alps to serene lakes. Its cities offer a blend of cultural richness, historical landmarks, and modern attractions. Known for its neutrality, high quality of life, and innovations in banking and finance, Switzerland is a top destination for those seeking both natural beauty and urban sophistication."
+    description: "Coming Soon"
+    // description: "Switzerland captivates visitors with its breathtaking landscapes, from the majestic Alps to serene lakes. Its cities offer a blend of cultural richness, historical landmarks, and modern attractions. Known for its neutrality, high quality of life, and innovations in banking and finance, Switzerland is a top destination for those seeking both natural beauty and urban sophistication."
   },
   {
     title: 'North America',
     image: 'https://images.unsplash.com/photo-1547815749-838c83787de2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    description: "These cities and countries offer a diverse array of experiences, from historic landmarks and cultural attractions to natural wonders and vibrant city life."
+    description: "Coming Soon"
+    // description: "These cities and countries offer a diverse array of experiences, from historic landmarks and cultural attractions to natural wonders and vibrant city life."
   },
   {
     title: 'Scandinavian countries',
     image: 'https://images.unsplash.com/photo-1507272931001-fc06c17e4f43?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    description: "These Scandinavian countries offer breathtaking natural landscapes, rich cultural heritage, and vibrant cities, making them ideal destinations for travelers seeking adventure, culture, and relaxation."
+    description: "Coming Soon"
+    // description: "These Scandinavian countries offer breathtaking natural landscapes, rich cultural heritage, and vibrant cities, making them ideal destinations for travelers seeking adventure, culture, and relaxation."
   },
   {
     title: 'United Kingdom',
     image: 'https://images.unsplash.com/photo-1454537468202-b7ff71d51c2e?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    description: "These destinations offer a multifaceted experience, from the academic allure of Oxford and Cambridge to the vibrant culture of Bristol and the historic charm of Cardiff. Glasgow adds a dynamic arts scene, while the Scottish Highlands, with the NC500, present nature's grandeur. Each destination is a testament to the UK's rich history, cultural diversity, and natural beauty, making them must-visit places for a comprehensive exploration of the United Kingdom."
+    description: "Coming Soon"
+    // description: "These destinations offer a multifaceted experience, from the academic allure of Oxford and Cambridge to the vibrant culture of Bristol and the historic charm of Cardiff. Glasgow adds a dynamic arts scene, while the Scottish Highlands, with the NC500, present nature's grandeur. Each destination is a testament to the UK's rich history, cultural diversity, and natural beauty, making them must-visit places for a comprehensive exploration of the United Kingdom."
   },
   {
     title: 'Japan',
     image: 'https://images.unsplash.com/photo-1545569341-9eb8b30979d9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    description: "Japan entices visitors with a harmonious blend of ancient traditions and cutting-edge modernity. From the neon-lit streets of Tokyo to the serene temples of Kyoto and the historic resilience of Hiroshima, Japan offers a diverse range of experiences. Its unique culture, picturesque landscapes, and technological innovations make it a top destination for those seeking a journey that seamlessly combines tradition and innovation."
+    description: "Coming Soon"
+    // description: "Japan entices visitors with a harmonious blend of ancient traditions and cutting-edge modernity. From the neon-lit streets of Tokyo to the serene temples of Kyoto and the historic resilience of Hiroshima, Japan offers a diverse range of experiences. Its unique culture, picturesque landscapes, and technological innovations make it a top destination for those seeking a journey that seamlessly combines tradition and innovation."
   },
 ]
 
--- src/components/home/hero.jsx
+++ src/components/home/hero.jsx
@@ -3,17 +3,18 @@ import Search from '../forms/search'
 
 export default function Hero() {
   return (
-    <section className='pt-[100px] h-[100vh] w-full bg-japan bg-cover'>
-      <div className='container mx-auto px-[5%] lg:px-0 h-full'>
-        <div className='h-full flex items-center text-black'>
-          <div className='flex flex-col max-w-[700px] gap-[24px]'>
-            <p className='font-bold text-[42px] lg:text-[75px] leading-[58px] lg:leading-[86px]'>
+    <section className='pt-[100px] h-[100vh] w-full relative'>
+      <div className='container mx-auto px-[5%] lg:px-0 h-full '>
+        <div className='h-full flex items-center text-white '>
+          <div className='flex flex-col max-w-[800px] gap-[24px] bg-custom-red bg-opacity-70 p-[20px] lg:p-[50px]'>
+            <p className='font-bold text-[36px] lg:text-[75px] leading-[42px] lg:leading-[86px]'>
               Discover Japan&apos;s <br />Hidden Gems
             </p>
-            <p className='text-[16px] md:text-[18px] leading-[34px] '>Embark on an unforgettable journey through the heart of Japan with our expertly curated tours. From the serene temples of Kyoto to the bustling streets of Tokyo, experience the perfect blend of tradition and modernity.</p>
+            <p className='text-[16px] md:text-[18px] leading-[24px] md:leading-[34px] text-white'>Embark on an unforgettable journey through the heart of Japan with our expertly curated tours. From the serene temples of Kyoto to the bustling streets of Tokyo, experience the perfect blend of tradition and modernity.</p>
             {/* <Search /> */}
           </div>
         </div>
+        <div className='absolute top-0 right-0 h-full w-full md:w-[75%] z-[-1] bg-japan bg-cover'></div>
       </div>
     </section>
   )
