31f989d69137741b765d296d53d65dd93fe70150
mohammedajmal <mdajmalmt@gmail.com>
2024-07-25 00:16:08 +0530

    transfers added back

---
+++ src/app/transfers/[transfers]/guide-card.jsx
@@ -0,0 +1,26 @@
+'use client'
+import Image from 'next/image';
+import React, { useState } from 'react';
+
+export default function TransferGuideCard({ data }) {
+
+  return (
+    <div className='w-full h-[200px] md:h-[368px] rounded-[10px] font-bold overflow-hidden flex items-end relative'>
+
+      <Image
+        src={data.image}
+        alt={data.name}
+        className={`w-full h-full object-cover `}
+        height={600}
+        width={600}
+      />
+
+      <div className='absolute bottom-0 h-[100px] w-full p-[15px] bg-[#00000080] text-white'>
+        <p className='uppercase'>{data.name}</p>
+        <p className='uppercase'>{data?.title}</p>
+
+      </div>
+
+    </div>
+  );
+}
