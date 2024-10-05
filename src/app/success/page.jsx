ebd769e00267fe7c06e6ee20051d25dd8b2e5bcc
Ajmal <frontend.fjordstans@gmail.com>
2024-10-05 21:24:38 +0530

    apos issue fixed

--- src/app/success/page.jsx
+++ src/app/success/page.jsx
@@ -5,7 +5,7 @@ export default function Page() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-3xl font-bold text-green-600 mb-4">Payment Successful</h1>
-      <p className="text-lg text-gray-600 text-center">Thank you for your payment. <br/> Check your email for invoice and you'll get the confirmation shortly</p>
+      <p className="text-lg text-gray-600 text-center">Thank you for your payment. <br/> Check your email for invoice and you&apos;ll get the confirmation shortly</p>
       <div className="mt-8">
         <Link href={{ pathname: '/', }}> 
         {/* query: { country: user.selectedCountry } */}
